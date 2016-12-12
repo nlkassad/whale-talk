e2261b50ad4feb62e5e65688132f562b78fc1d19
nlkassad <nlkassad@hotmail.com>
2016-12-12 14:38:22 -0500

    Add Whale Talk function from CA lesson
    - add prompt for text to translate
    - add check for vowels
    - add u and e duplication

---
+++ whaleTalk.js
@@ -0,0 +1,18 @@
+var input = prompt('What would you like to translate into whale talk?');
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+var resultArray = [];
+
+for (var i = 0; i < input.length; i++) {
+  for (var j = 0; j < vowels.length; j++) {
+    if(input[i] === vowels[j]) {
+      resultArray.push(input[i]);
+    }
+  }
+  if (input[i] === 'e') {
+     resultArray.push(input[i]);
+  }
+  if (input[i] === 'u') {
+     resultArray.push(input[i]);
+  }
+}
+console.log(resultArray.join('').toUpperCase());
